95c602ec7a1213f18a244498f947bfdc68ef211e
YellowCat98 <elcucuyisgay@gmail.com>
2024-10-01 20:00:54 +0100

    bind notification and its enum

--- src/Serpent.cpp
+++ src/Serpent.cpp
@@ -1,7 +1,22 @@
 #include "Serpent.hpp"
 
 namespace py = pybind11;
+using namespace geode::prelude;
 
-void Serpent::geode::enums(py::module& m) {
+void Serpent::_geode::bind(py::module& m) {
+    py::class_<Notification>(m, "Notification")
+        .def_static("create", static_cast<Notification* (*)(const std::string&, NotificationIcon, float)>(&Notification::create), py::arg("text"), py::arg("icon") = NotificationIcon::None, py::arg("time") = geode::NOTIFICATION_DEFAULT_TIME, py::return_value_policy::reference)
+        .def("show", &Notification::show)
+        .def("cancel", &Notification::cancel)
+        .def("hide", &Notification::hide);
+}
 
+void Serpent::_geode::enums(py::module& m) {
+    py::enum_<NotificationIcon>(m, "NotificationIcon")
+        .value("None", NotificationIcon::None)
+        .value("Loading", NotificationIcon::Loading)
+        .value("Success", NotificationIcon::Success)
+        .value("Warning", NotificationIcon::Warning)
+        .value("Error", NotificationIcon::Error)
+        .value("Info", NotificationIcon::Info);
 }
--- src/Serpent.hpp
+++ src/Serpent.hpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 
 namespace Serpent {
-    struct geode {
+    struct _geode { // prefixed with _ so it doesnt get confused between Serpent::geode and geode ns
         static void bind(pybind11::module& m);
         static void enums(pybind11::module& m);
     };
